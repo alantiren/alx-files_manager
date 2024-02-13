bd4191477677545e69d920d33c824685f86cbf67
alantiren <tirenalan0@gmail.com>
2024-02-13 19:47:00 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/index.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
